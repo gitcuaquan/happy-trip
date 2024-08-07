336d0db9839ce69e2fe298f536ee5229263c2ae6
Hunter X <79095757+gitcuaquan@users.noreply.github.com>
2024-08-08 00:26:21 +0700

    update button

--- layouts/blank.vue
+++ layouts/blank.vue
@@ -3,13 +3,22 @@
 </script>
 
 <template>
-  <fontend-header/>
+  <fontend-header />
   <div>
-    <slot/>
+    <slot />
   </div>
-  <fontend-footer/>
+  <fontend-footer />
+  <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 999999999999">
+    <a target="_blank" href="https://zalo.me/0972970000" class="mx-1">
+      <img width="35" src="/img/icon-zalo.svg" alt="">
+    </a>
+    <a target="_blank" href="https://www.facebook.com/share/saJiXTEkoVHMig49/?mibextid=LQQJ4d" class="mx-1">
+      <img width="30" src="/img/icon-mess.svg" alt="">
+    </a>
+    <a target="_blank" href="tel:0972970000" class="mx-1">
+      <img width="30" src="/img/phone-call-icon.svg" alt="">
+    </a>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
