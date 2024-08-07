73abe75dfa20dc2a1513baabc789138efb020f7c
hoai son <hoaisonbk@gmail.com>
2024-08-08 00:03:14 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,36 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/styles/main.scss'],
-  build: {
-    transpile: ['@vuepic/vue-datepicker']
+  app: {
+    head: {
+      link: [{ rel: "icon", type: "image/x-icon", href: "/img/logohpt.png" }],
+    },
   },
-  modules: ['nuxt-aos', 'nuxt-gtag', ["nuxt-mail",{
-    message: {
-      to: 'happytripexpress@gmail.com',
-    },
-    smtp: {
-      host: "smtp.gmail.com",
-      port: 587,
-      auth: {
-        user: 'happytripexpress@gmail.com',
-        pass: 'xbxc lmci kldt lusk',
+  css: ["~/assets/styles/main.scss"],
+  build: {
+    transpile: ["@vuepic/vue-datepicker"],
+  },
+  modules: [
+    "nuxt-aos",
+    "nuxt-gtag",
+    [
+      "nuxt-mail",
+      {
+        message: {
+          to: "happytripexpress@gmail.com",
+        },
+        smtp: {
+          host: "smtp.gmail.com",
+          port: 587,
+          auth: {
+            user: "happytripexpress@gmail.com",
+            pass: "xbxc lmci kldt lusk",
+          },
+        },
       },
-    },
-  }]],
-
-})
+    ],
+  ],
+  gtag: {
+    id: "AW-16661686858",
+  },
+});
